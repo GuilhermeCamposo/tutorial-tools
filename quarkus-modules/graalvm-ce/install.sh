b57eafe43a82427e0645c61b72ae80bb48c15c4e
Guilherme Camposo <guilherme.camposo@me.com>
2021-09-20 13:09:46 -0300

    downgrading camelk cli and removing native-image

--- quarkus-modules/graalvm-ce/install.sh
+++ quarkus-modules/graalvm-ce/install.sh
@@ -6,6 +6,6 @@ set -o pipefail
 
 SOURCES_DIR=/tmp/artifacts
 
-mkdir -p /opt/graalvm 
+mkdir -p /opt/graalvm
 tar -xzf ${SOURCES_DIR}/graalvm-ce-amd64.tar.gz -C /opt/graalvm --strip-components=1
-/opt/graalvm/bin/gu install native-image
+#/opt/graalvm/bin/gu install native-image
