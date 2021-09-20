52cdd699e2057a75ebde2221f9c7d30edb711787
Guilherme Camposo <guilherme.camposo@me.com>
2021-09-20 09:31:22 -0300

    fix script

--- clients-modules/utils/install.sh
+++ clients-modules/utils/install.sh
@@ -10,8 +10,8 @@ SOURCES_DIR=/tmp/artifacts
 
 cp ${SOURCES_DIR}/yq /usr/local/bin/yq
 cp ${SOURCES_DIR}/jq /usr/local/bin/jq
-cp ${SOURCES_DIR}/stern /usr/local/bin/stern
-cp ${SOURCES_DIR}/hey /usr/local/bin/hey
+# cp ${SOURCES_DIR}/stern /usr/local/bin/stern
+# cp ${SOURCES_DIR}/hey /usr/local/bin/hey
 
 cp -v ${SCRIPT_DIR}/bin/run.sh /usr/local/bin/
 
@@ -20,4 +20,6 @@ chmod +x /usr/local/bin/run.sh
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubectx -o  /usr/local/bin/kubectx
 curl https://raw.githubusercontent.com/ahmetb/kubectx/master/kubens -o  /usr/local/bin/kubens
 
-chmod +x /usr/local/bin/yq /usr/local/bin/jq /usr/local/bin/stern /usr/local/bin/hey /usr/local/bin/kubectx /usr/local/bin/kubens
+# chmod +x /usr/local/bin/yq /usr/local/bin/jq /usr/local/bin/stern /usr/local/bin/hey /usr/local/bin/kubectx /usr/local/bin/kubens
+
+chmod +x /usr/local/bin/yq /usr/local/bin/jq  /usr/local/bin/kubectx /usr/local/bin/kubens
